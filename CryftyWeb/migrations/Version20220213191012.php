a6e13f431987da5f0c6f7c7892eaea9a52a72103
zarga <mouhamedamine.benelazrak@esprit.tn>
2022-02-18 11:21:44 +0100

    blog article +crud+c saisi

---
+++ CryftyWeb/migrations/Version20220213191012.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220213191012 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE blog_article CHANGE subtitre subtitle VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE admin CHANGE first_name first_name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE blog_article ADD subtitre VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, DROP subtitle, CHANGE title title VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE text text VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE client CHANGE first_name first_name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE last_name last_name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE email email VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE phone_number phone_number VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE address address VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE moderator CHANGE first_name first_name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE user CHANGE username username VARCHAR(180) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE roles roles LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:json)\', CHANGE password password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE type type VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
--- CryftyWeb/src/Entity/Blog/BlogComment.php
+++ CryftyWeb/src/Entity/Blog/BlogComment.php
@@ -4,6 +4,7 @@ namespace App\Entity\Blog;
 
 use App\Repository\BlogCommentRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=BlogCommentRepository::class)
@@ -21,4 +22,59 @@ class BlogComment
     {
         return $this->id;
     }
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+    public function getUname(): ?string
+    {
+        return $this->username;
+    }
+
+
+    public function setUname(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $comment;
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+    /**
+     * @Assert\DateTime()
+     * @ORM\Column(type="datetime")
+     */
+    private $dateC;
+    public function getDateC(): ?string
+    {
+        return $this->dateC;
+    }
+
+    public function setDateC(string $dateC): self
+    {
+        $this->dateC = $dateC;
+
+        return $this;
+    }
+
+
 }
